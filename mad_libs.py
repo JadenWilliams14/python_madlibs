2af91998e7437bae548f25a8ca4635d1addc64a6
JadenWilliams14 <willia14@alumni.southwestern.edu>
2025-04-25 17:15:57 -0500

    Add Replay Feature
    Updated mad_libs.py to include the ability to replay the game.

--- mad_libs.py
+++ mad_libs.py
@@ -1,21 +1,31 @@
 # Python Mad Libs Warm-Up Activity
 
-# Welcome message
-print("Welcome to Python Mad Libs!")
-print("Answer the following questions to create your very own silly story.\n")
+# The Game will repeat until it is broken out of
+while True:
 
-# Gather user inputs
-adjective = input("Enter an adjective: ")
-noun = input("Enter a noun: ")
-verb = input("Enter a verb: ")
-adverb = input("Enter an adverb: ")
+    # Welcome message
+    print("\nWelcome to Python Mad Libs!")
+    print("Answer the following questions to create your very own silly story.\n")
 
-# Build the story using an f-string
-story = (
-    f"Today, I saw a {adjective} {noun} that decided to {verb} {adverb}.\n"
-    "I couldn't believe my eyes!"
-)
+    # Gather user inputs
+    adjective = input("Enter an adjective: ")
+    noun = input("Enter a noun: ")
+    verb = input("Enter a verb: ")
+    adverb = input("Enter an adverb: ")
 
-# Display the completed story
-print("\nHere is your story:")
-print(story)
+    # Build the story using an f-string
+    story = (
+        f"Today, I saw a {adjective} {noun} that decided to {verb} {adverb}.\n"
+        "I couldn't believe my eyes!"
+    )
+
+    # Display the completed story
+    print("\nHere is your story:")
+    print(story)
+    play_again = input("Do you want to play again? (If so, type yes): ")
+
+    # leaves the while loop if the user answers anything besides 'yes'
+    if play_again != 'yes':
+        break
+
+print('\nFarewell! Thank you for playing!')
