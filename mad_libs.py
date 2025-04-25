7e30d1477dd96c053684a6c8bdca1e337f30a93e
JadenWilliams14 <willia14@alumni.southwestern.edu>
2025-04-25 17:55:02 -0500

    List of input for mad_libs.py
    Added the additional feature of asking user to provide a list and added an outline for the story

--- mad_libs.py
+++ mad_libs.py
@@ -1,4 +1,6 @@
 # Python Mad Libs Warm-Up Activity
+# import to use random on list of words.
+import random
 
 # The Game will repeat until it is broken out of
 while True:
@@ -8,20 +10,29 @@ while True:
     print("Answer the following questions to create your very own silly story.\n")
 
     # Gather user inputs
-    adjective = input("Enter an adjective: ")
-    noun = input("Enter a noun: ")
-    verb = input("Enter a verb: ")
-    adverb = input("Enter an adverb: ")
+    adjective = input("Enter a comma seperated list of adjectives: ")
+    noun = input("Enter a comma seperated list of nouns: ")
+    verb = input("Enter a comma seperated list of verbs: ")
+    adverb = input("Enter a comma seperated list of adverbs: ")
 
     # Build the story using an f-string
+    # random.choice is selecting a random option from the list that is created by splitting on the commas
     story = (
-        f"Today, I saw a {adjective} {noun} that decided to {verb} {adverb}.\n"
+        f"Today, I saw a {random.choice(adjective.split(','))} {random.choice(noun.split(','))} that decided to {random.choice(verb.split(','))} {random.choice(adverb.split(','))}.\n"
         "I couldn't believe my eyes!"
     )
 
     # Display the completed story
     print("\nHere is your story:")
+    print("""
+*************************************************************
+          """)
     print(story)
+    print("""
+*************************************************************
+          """)
+
+    # Take user input on if they want to play again
     play_again = input("Do you want to play again? (If so, type yes): ")
 
     # leaves the while loop if the user answers anything besides 'yes'
